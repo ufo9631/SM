68de84ef82d7287ad33200ac00933cb085e3a830
编程猴子无大脑 <695901733@qq.com>
2018-07-10 23:21:41 +0800

    111

--- SM.Web/SM.Web/Startup.cs
+++ SM.Web/SM.Web/Startup.cs
@@ -1,7 +1,12 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
+using AspectCore.Extensions.Autofac;
+using Autofac;
+using Autofac.Configuration;
+using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -17,11 +22,24 @@ namespace SM.Web
         }
 
         public IConfiguration Configuration { get; }
-
+        public IContainer ApplicationContainer { get; private set; }
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public IServiceProvider ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            var builder = new ContainerBuilder();
+            builder.Populate(services);
+
+            var dataDal = Assembly.Load("DAL");
+            builder.RegisterAssemblyTypes(dataDal).Where(a => a.FullName.Contains("DAL")).AsImplementedInterfaces();
+            var dataBll = Assembly.Load("BLL");
+            builder.RegisterAssemblyTypes(dataBll).Where(a => a.FullName.Contains("BLL")).AsImplementedInterfaces();
+
+            var module = new ConfigurationModule(Configuration);
+            builder.RegisterModule(module);
+
+            ApplicationContainer = builder.Build();
+            return new AutofacServiceProvider(ApplicationContainer);//第三方IOC接管 core内置DI容器
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
