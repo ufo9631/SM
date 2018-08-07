7413757b238b13391f575ffcc4324f855aa271de
编程猴子无大脑 <695901733@qq.com>
2018-08-07 23:12:25 +0800

    111

---
+++ SM.Web/SM.IDAL/IRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SM.IDAL
+{
+    public interface IRepository<TEntity>
+    {
+        #region 属性
+        IQueryable<TEntity> Entities { get; }
+        #endregion
+
+        #region 公共方法
+        int Insert(TEntity entity);
+
+        int Insert(IEnumerable<TEntity> entities);
+
+        int Delete(object id);
+
+        int Delete(TEntity entity);
+
+        int Delete(IEnumerable<TEntity> entities);
+
+        int Update(TEntity entity);
+
+        TEntity GetByKey(object key);
+        #endregion
+    }
+}
